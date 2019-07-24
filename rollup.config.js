b9a171219ae4cee3e0a3151bf06e200bc96ecdff
McKayla Washburn <partheseas@gmail.com>
2019-07-24 10:17:18 -0600

    ESLint-ify and such

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,17 @@ import minify from 'rollup-plugin-babel-minify'
 
 export default [{
     input: 'electron/app.js',
+    external: [
+        'drivelist',
+        'electron',
+        'electron-squirrel-startup',
+        'events',
+        'fs',
+        'gardens',
+        'path',
+        'url',
+        'source-map-support/register'
+    ],
     plugins: [ minify({ comments: false }) ],
     output: {
         file: 'target/app.js',
@@ -12,6 +23,12 @@ export default [{
     }
 }, {
     input: 'client/ipc.js',
+    external: [
+        'electron',
+        'gardens',
+        'react',
+        'react-dom'
+    ],
     plugins: [
         babel({
             presets: [ babel_preset_react ]
