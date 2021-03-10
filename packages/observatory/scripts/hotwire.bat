671c7a2db313e41749d9ca5c2d48795da2cba751
McKayla Washburn <mckayla@hey.com>
2021-03-10 08:34:13 -0700

    windows?

---
+++ packages/observatory/scripts/hotwire.bat
@@ -0,0 +1,10 @@
+: This patches over how badly electron-forge handles workspaces...
+
+: Define a function we can call to actually hotwire things
+echo "Linking..."
+
+: mklink /d ..\..\node_modules node_modules
+mklink /d ..\..\..\node_modules\drivelist node_modules\drivelist
+mklink /d ..\..\..\node_modules\electron node_modules\electron
+
+echo "Success!"
