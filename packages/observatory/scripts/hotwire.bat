6b31de26ed806fb036232042bfa8bda37884d114
McKayla Washburn <mckayla@hey.com>
2021-03-10 09:56:05 -0700

    :shrug:

--- packages/observatory/scripts/hotwire.bat
+++ packages/observatory/scripts/hotwire.bat
@@ -3,8 +3,9 @@
 : Define a function we can call to actually hotwire things
 echo "Linking..."
 
-: mklink /d ..\..\node_modules node_modules
-mklink /d node_modules\drivelist  ..\..\..\node_modules\drivelist
-mklink /d node_modules\electron ..\..\..\node_modules\electron
+rm -r node_modules
+mklink /d node_modules ..\..\node_modules
+: mklink /d node_modules\drivelist  ..\..\..\node_modules\drivelist
+: mklink /d node_modules\electron ..\..\..\node_modules\electron
 
 echo "Success!"
